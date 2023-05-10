edf1e9bbed3439d23a4d1d5bf96fa50850b68705
cyberdslayer <95081360+cyberdslayer@users.noreply.github.com>
2023-05-10 23:58:46 +0530

    Initialized repo and created simple 2d array

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter
+
+class ChessBoard:
+    def __init__(self):
+        self.board = [
+            ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ["P", "P", "P", "P", "P", "P", "P", "P"],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            ["r", "n", "b", "q", "k", "b", "n", "r"]
+        ]
+
+
+
+    def display_board(self):
+        for row in self.board:
+            print(row)
+
+
+    def move(self, start, end):
+        
