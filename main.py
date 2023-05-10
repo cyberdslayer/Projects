dea4b474d43c427fbcf92f91e9d7f6707987a917
cyberdslayer <95081360+cyberdslayer@users.noreply.github.com>
2023-05-11 00:38:36 +0530

    removed tkinter, added comments, move, valid move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tkinter
+
 
 class ChessBoard:
+    # Construtor for 2D array
     def __init__(self):
         self.board = [
             ["R", "N", "B", "Q", "K", "B", "N", "R"],
@@ -12,13 +13,26 @@ class ChessBoard:
             ["p", "p", "p", "p", "p", "p", "p", "p"],
             ["r", "n", "b", "q", "k", "b", "n", "r"]
         ]
-
-
-
+    # Defining display 
     def display_board(self):
         for row in self.board:
             print(row)
 
-
+    # Defining the move function for every 
     def move(self, start, end):
+        start_x, start_y = start
+        end_x, end_y = end
+        piece = self.board[start_x][start_y]
+        if not self.is_valid_move(piece, start, end):
+            return False
+
+        self.board[start_x][start_y] = " "
+        self.board[end_x][end_y] = piece
+        return True
+
+    def is_valid_move(self, piece, start, end):
+        # checking if the move is within the bound of the board
+        if end[0] < 0 or end[0] > 7 or end[1] <0 or end[1] > 7 :
+            return False
+
         
